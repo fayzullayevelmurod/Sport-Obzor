c3eeaaae6d47174b8ab12eafac78f741bc2ea5f2
QobilovSardor <note47297@gmail.com>
2024-02-19 13:38:33 +0500

    home page 80% finished

--- js/script.js
+++ js/script.js
@@ -1,29 +1,65 @@
 // script.js
 $(document).ready(function () {
 	const giftBoxCard = $(".gift-box__card");
-	$(giftBoxCard).hide();
 
 	$(".gift-box__img").click(function () {
-		$(giftBoxCard).fadeToggle(100);
+		$(giftBoxCard).toggleClass('open');
 	});
 	$(".close-btn").click(function () {
-		$(giftBoxCard).fadeToggle(100);
+		$(giftBoxCard).toggleClass('open');
 	})
 
-	// Acordion
+	// header menu
+	$('.header-bottom .down-box').hover(function () {
+		$('.header__drop-list').addClass('drop-list-nav');
+	}, function () {
+		$('.header__drop-list').removeClass('drop-list-nav');
+	}
+	);
+
+	$('.header__drop-list').hover(function () {
+		$(this).addClass('drop-list-nav');
+	}, function () {
+		$(this).removeClass('drop-list-nav');
+	}
+	);
+
+	// // Acordion
 	const downBoxHide = document.querySelectorAll(".down-box__hide")
 	const downBox = document.querySelectorAll(".down-box");
 	const upBoxBtn = document.querySelectorAll(".up-box__btn");
+	const accordion = document.querySelectorAll('.accordion');
 	$(downBoxHide).hide();
 
-	$(upBoxBtn).each(function (index, item) {
-		$(item).click(function () {
-			$(this).toggleClass('active-icon');
-			$(downBox).slideToggle('fast');
-			$(downBoxHide).slideToggle('fast');
-		})
-	});
+	// const accordions = $('.acordion');
+	// console.log(accordions);
+	// accordions.each(function () {
+	// 	const downBoxHide = $(this).find('.down-box__hide');
+	// 	const downBox = $(this).find('.down-box');
+	// 	const upBoxBtn = $(this).find('.up-box__btn');
+	// 	// Hide all down boxes initially
+	// 	downBoxHide.hide();
+
+	// 	// Click event for each button in the accordion
+	// 	upBoxBtn.click(function () {
+	// 		// Toggle the 'active-icon' class
+	// 		$(this).toggleClass('active-icon');
+
+	// 		// Toggle visibility of the corresponding down box with animation
+	// 		downBox.show('fast');
+
+	// 		// Hide other down boxes and remove 'active-icon' class
+	// 		accordions.not($(this).closest('.accordion')).find('.down-box__hide, .down-box').slideUp('fast');
+	// 		upBoxBtn.not(this).removeClass('active-icon');
+	// 	});
+	// });
 
 
+	// if (item === index) {
+	// 	$(item).click(function () {
+	// 		$(this).toggleClass('active-icon');
+	// 		$(downBox).slideToggle('fast');
+	// 		$(downBoxHide).slideToggle('fast');
+	// 	})
+	// }
 });
-
